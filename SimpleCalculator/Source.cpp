183adb68774268fd2826eb37c52b66564e07c155
Eric Hall <eharper8367@gmail.com>
2017-04-20 15:22:11 -0400

    Upload

---
+++ SimpleCalculator/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int sum;
+
+	cout << "Enter a number \n";
+	cin >> a;
+	
+	cout << "Enter another number \n";
+	cin >> b;
+
+	sum = a + b;
+	cout << "The sum of those numbers is: " << sum << endl;
+
+	system("pause");
+	return 0;
+}
